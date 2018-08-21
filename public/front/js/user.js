314105f3a3d50099b7e75df69deb46cd8587b5f3
lpfg-3 <l3130389175@gmail.com>
2018-08-21 19:12:51 +0800

    用户页完成

---
+++ public/front/js/user.js
@@ -0,0 +1,36 @@
+$(function () {
+  
+  // 发送请求获取用户数据
+  $.ajax({
+    url: '/user/queryUserMessage',
+    dataType: 'json',
+    success: function ( info ) {
+      console.log(info);
+      if (info.error === 400) {
+        location.href = 'login.html';
+      }
+      var htmlStr = template('tpl', info);
+      $('#userInfo').html( htmlStr );
+    }
+
+  })
+
+
+  // 退出登陆
+  $('#logout').click(function () {
+    // 发送请求,退出登陆
+    $.ajax({
+      url: '/user/logout',
+      dataType: 'json',
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          location.href = 'login.html';
+        }
+        
+      }
+    })
+  })
+
+
+})
